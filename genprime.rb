d231f3061b8317037a36a348ee7465a73d0b6db4
Steven Noonan <steven@uplinklabs.net>
2008-11-10 21:10:54 -0800

    ruby: add genprime implementation
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

---
+++ genprime.rb
@@ -0,0 +1,56 @@
+#!/usr/bin/ruby
+
+def isprime(x)
+	if x < 2
+		return 0
+	end
+	if x < 4
+		return 1
+	end
+	if x == 5
+		return 1
+	end
+	if x % 2 == 0
+		return 0
+	end
+	if x % 5 == 0
+		return 0
+	end
+	if (x + 1) % 6 != 0
+		if (x - 1) % 6 != 0
+			return 0
+		end
+	end
+	lim=Math.sqrt(x).to_i+1
+	y = 3
+	while y <= lim
+		if x % y == 0
+			return 0
+		end
+		y += 2
+	end
+	return 1
+end
+
+def genprime(max)
+	count = 0
+	current = 1
+	while count<max
+		if isprime(current)
+			count += 1
+		end
+		current += 1
+	end
+end
+
+start = ARGV[0].to_i
+stop = ARGV[1].to_i
+i = start
+until i > stop
+	starttime = Time.now
+	genprime(i)
+	endtime = Time.now
+	duration = endtime - starttime
+	printf "Found %d primes in %f seconds\n", i, duration
+	i += start
+end
