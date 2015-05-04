2bbbdf6dba3da476a416f424cdf543a6e1244e92
Steven Noonan <steven@uplinklabs.net>
2015-05-04 10:31:27 -0700

    genprime.rb: convert tabs to spaces
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- genprime.rb
+++ genprime.rb
@@ -1,57 +1,57 @@
 #!/usr/bin/ruby
 
 def isprime(x)
-	if x < 2
-		return 0
-	end
-	if x == 2
-		return 1
-	end
-	if x % 2 == 0
-		return 0
-	end
-	if x < 9
-		return 1
-	end
-	if (x + 1) % 6 != 0
-		if (x - 1) % 6 != 0
-			return 0
-		end
-	end
-	lim=Math.sqrt(x).to_i+1
-	y = 3
-	while y <= lim
-		if x % y == 0
-			return 0
-		end
-		y += 2
-	end
-	return 1
+    if x < 2
+        return 0
+    end
+    if x == 2
+        return 1
+    end
+    if x % 2 == 0
+        return 0
+    end
+    if x < 9
+        return 1
+    end
+    if (x + 1) % 6 != 0
+        if (x - 1) % 6 != 0
+            return 0
+        end
+    end
+    lim=Math.sqrt(x).to_i+1
+    y = 3
+    while y <= lim
+        if x % y == 0
+            return 0
+        end
+        y += 2
+    end
+    return 1
 end
 
 def genprime(max)
-	count = 0
-	current = 1
-	while count<max
-		if isprime(current) != 0
-			count += 1
-		end
-		current += 1
-	end
-	return current - 1
+    count = 0
+    current = 1
+    while count<max
+        if isprime(current) != 0
+            count += 1
+        end
+        current += 1
+    end
+    return current - 1
 end
 
 start = ARGV[0].to_i
 stop = ARGV[1].to_i + 1
 i = start
 if start == 0
-	Process.exit
+    Process.exit
 end
 until i >= stop
-	starttime = Time.now
-	last = genprime(i)
-	endtime = Time.now
-	duration = endtime - starttime
-	printf "Found %8d primes in %10.5f seconds (last was %10d)\n", i, duration, last
-	i += start
+    starttime = Time.now
+    last = genprime(i)
+    endtime = Time.now
+    duration = endtime - starttime
+    printf "Found %8d primes in %10.5f seconds (last was %10d)\n", i, duration, last
+    i += start
 end
